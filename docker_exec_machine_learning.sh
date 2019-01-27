bf706801525c69ef9d1917f34fab11aeb18240d2
makotomurakami <murakami_m@toyo.jp>
2019-01-27 15:04:46 +0900

    sh

---
+++ docker_exec_machine_learning.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -ti -e COLUMNS=86 -e LINES=104 machine_learning /bin/bash
---
+++ docker_run_machine_learning_bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+home=$(echo ~)
+pwd=$(pwd)
+user=$(id -un)
+docker run -ti --rm --runtime=nvidia -e DISPLAY=$DISPLAY -e XMODIFIERS=$XMODIFIERS -e COLUMNS=86 -e LINES=104 -v /tmp/.X11-unix/:/tmp/.X11-unix -v $home:$home -w $pwd -u $user --privileged -v /dev/video0:/dev/video0 --name machine_learning machine_learning /bin/bash
